b91856295babdaf80b71ce433c1f691a8faeb9ae
Dorian <doriancursos@gmail.com>
2024-03-01 13:53:07 +0100

    reducer notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { useReducer } from 'react';
+import AddNote from './components/add-note/AddNote';
+import NotesList from './components/notes-list/NotesList';
+import { notesReducer } from './reducers/notes-reducer';
 import { GlobalStyles } from './styles/GlobalStyles';
 
 const App = () => {
+	const [notes, dispatch] = useReducer(notesReducer, []);
+	// const [cart, setCart] = useState({
+	// 	items: [],
+	// 	totalPrice: '',
+	// 	discount:''
+	// })
+
+	// setCart({...cart, items:[...cart.items, item ]})
+	// dispatch({type:'ADD_ITEM', payload:item})
+	// dispatch({type:'REMOVE_ITEM', payload:item.id})
 	return (
 		<>
 			<GlobalStyles />
-			<h1>Núcleo de la aplicación</h1>
+			<h1>Notes</h1>
+			<AddNote dispatch={dispatch} />
+			<NotesList notes={notes} dispatch={dispatch} />
 		</>
 	);
 };
